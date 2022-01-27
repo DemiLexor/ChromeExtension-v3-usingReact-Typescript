0d97f96201137c18905c81911ce4203ae03d4da3
JoseCGDEV <contacto.cgd.jose182@gmail.com>
2022-01-26 23:17:32 -0600

    firs=upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,84 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { DOMMessage, DOMMessageResponse } from './types';
 
 function App() {
+  const [title, setTitle] = React.useState('');
+  const [headlines, setHeadlines] = React.useState<string[]>([]);
+  const [anchors, setAnchors] = React.useState<string[]>([]);
+
+  React.useEffect(() => {
+    /**
+     * We can't use "chrome.runtime.sendMessage" for sending messages from React.
+     * For sending messages from React we need to specify which tab to send it to.
+     */
+    chrome.tabs && chrome.tabs.query({
+      active: true,
+      currentWindow: true
+    }, tabs => {
+      /**
+       * Sends a single message to the content script(s) in the specified tab,
+       * with an optional callback to run when a response is sent back.
+       *
+       * The runtime.onMessage event is fired in each content script running
+       * in the specified tab for the current extension.
+       */
+      chrome.tabs.sendMessage(
+        tabs[0].id || 0,
+        { type: 'GET_DOM' } as DOMMessage,
+        (response: DOMMessageResponse) => {
+          setTitle(response.title);
+          setHeadlines(response.headlines);
+          setAnchors(response.anchors);
+        });
+      });
+      
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Simply React Extension Skeleton</h2>
+
+      <ul className="SEOForm">
+        <li className="SEOValidation">
+          <div className="SEOValidationField">
+            <span className="SEOValidationFieldTitle">Title</span>
+            <span className={`SEOValidationFieldStatus ${title.length < 30 || title.length > 65 ? 'Error' : 'Ok'}`}>
+              {title.length} Characters
+            </span>
+          </div>
+          <div className="SEOVAlidationFieldValue">
+            {title}
+          </div>
+        </li>
+
+        <li className="SEOValidation">
+          <div className="SEOValidationField">
+            <span className="SEOValidationFieldTitle">Main Heading</span>
+            <span className={`SEOValidationFieldStatus ${headlines.length !== 1 ? 'Error' : 'Ok'}`}>
+              {headlines.length}
+            </span>
+          </div>
+          <div className="SEOVAlidationFieldValue">
+            <ul>
+              {headlines.map((headline, index) => (<li key={index}>{headline}</li>))}
+            </ul>
+          </div>
+        </li>
+
+        <li className="SEOValidation">
+          <div className="SEOValidationField">
+            <span className="SEOValidationFieldTitle">Links on page</span>
+            <span className={`SEOValidationFieldStatus ${anchors.length !== 50 ? 'Error' : 'Ok'}`}>
+              {anchors.length}
+            </span>
+          </div>
+          <div className="SEOVAlidationFieldValue">
+          </div>
+        </li>
+
+
+      </ul>
     </div>
   );
 }
---
+++ src/chromeServices/DOMEvaluator.ts
@@ -0,0 +1,20 @@
+import { DOMMessage, DOMMessageResponse } from '../types';
+
+const messagesFromReactAppListener = (msg: DOMMessage, sender: chrome.runtime.MessageSender, sendResponse: (response: DOMMessageResponse) => void) => {
+  console.log('[content.js]. Message received', msg);
+
+  const response: DOMMessageResponse = {
+    title: document.title,
+    headlines: Array.from(document.getElementsByTagName<"h1">("h1")).map(h1 => h1.innerText),
+    anchors: Array.from(document.getElementsByTagName<"a">("a")).map(a => a.innerText)
+  };
+
+  console.log('[content.js]. Message response', response);
+
+  sendResponse(response)
+}
+
+/**
+ * Fired when a message is sent from either an extension process or a content script.
+ */
+chrome.runtime.onMessage.addListener(messagesFromReactAppListener);
